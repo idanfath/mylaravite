3898df1d35c19025cd32af578d81546cab29f298
idanfath <arfounded@gmail.com>
2024-01-16 09:05:46 +0800

    firstcommit

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import app from "./app.vue"
+
+//this is components, mount it to and id. to call it, simply give something the corresponding id
+
+createApp(app).mount("#app") //to call it, <div id="app"></div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Appraisal</title>
+        @vite(['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div class="test test2">
+            <h1>hello</h1>
+        </div>
+        <div id="app"></div>
+    </body>
+</html>
